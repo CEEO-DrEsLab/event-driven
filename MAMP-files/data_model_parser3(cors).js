991b64ba3b95e9112c7d1f03ec06f96077c278b3
BenZackin <ben.zackin@gmail.com>
2016-07-29 15:27:07 -0400

    fixed set instruction parsing and put in loop delay for get calls

--- MAMP-files/data_model_parser3(cors).js
+++ MAMP-files/data_model_parser3(cors).js
@@ -1,48 +1,59 @@
 // Event-Driven EV3 Data Parser: CORS
 // By: Benjamin Zackin
-// Last Modified: 7/28/16
+// Last Modified: 7/29/16
 // Notes: loops through all triggers, asking if they are true. if one is, all
 // 		the corresponding actions are set on the brick
 //		Added object wrapper to this file
-modelParse = {
+var modelParse = {
 	url: "http://130.64.94.159:5000",
 	lastValues: {},
 	config: {},
-	trigger_list: {},
-	currentValues: {},
+	trigger_list: [],
+	delayedLoop: {},
 	parse_data: function (dataFile) { 
 		//Start Data Parsing
 		var data = JSON.parse(dataFile);
-		console.log("parse_data Fcn://\tJSON parsed to variable 'data'");
+		//console.log("parse_data Fcn://\tJSON parsed to variable 'data'");
 		var raw_config = data.portConfig;
-		var trigger_list = data.triggers;
+		for (var d = 0; d < data.triggers.length; d++) {}
+		this.trigger_list = data.triggers;
 		for (portID in raw_config) { // truncate portConfig to only contain assigned ports
 			if (raw_config[portID] != "None") {
-				config[portID] = raw_config[portID];
+				this.config[portID] = raw_config[portID];
 			}
 		}
-		console.log("parse_data Fcn://\tport config truncated");
-		var trigger_len = trigger_list.length;
+		//console.log("parse_data Fcn://\tport config truncated");
+		var trigger_len = this.trigger_list.length;
 		// preset all the old values (so we can detect state changes!)
 		for(var i = 0; i < trigger_len; i++) {
-			send_get(config[trigger_list[i].port], trigger_list[i], i, true);
+			this.send_get(this.config[this.trigger_list[i].port], this.trigger_list[i], i, true);
 		}
-		console.log("parse_data Fcn://\tstart values for all triggers set");
-		while(true) {
-			for(var j = 0; j < trigger_len; j++) {
-				console.log("parse_data Fcn://\t about to send get for trigger #: " + j);
-				send_get(config[trigger_list[j].port], trigger_list[j],j,false);
-				console.log("parse_data Fcn://\tget sent for peripheral #: " + j + " type: " + config[trigger_list[j].port] + "!!!!!");
-				if (is_state_change(currentValues[trigger_list[j]],trigger_list[j],lastValues[trigger_list[j]])) {
-					console.log("parse_data Fcn://\t state change detected in: " + config[trigger_list[j].port]);
-					lastValues[trigger_list[j]] = currentValues[trigger_list[j]]; // if a state change occurs, change the last value to be the current value.
-					send_set(config,trigger_list[j].actions);
-					console.log("parse_data Fcn://\t set sent for all corresponding actions! Woohoo!!!!");
+		//console.log("parse_data Fcn://\tstart values for all triggers set");
+		var j = 0;
+		// while(true) {
+		// 	console.log("parse_data Fcn://\tinfinite loop entered");
+		// 	j = 0;
+			delayedLoop = setInterval(function(){ 
+				//console.log("parse_data Fcn://\t delayed loop entered, iteration: " + j);
 
+				if(j < trigger_len) {
+				 	//console.log("parse_data Fcn://\t about to send get for trigger #: " + j);
+					modelParse.send_get(modelParse.config[modelParse.trigger_list[j].port], modelParse.trigger_list[j], j, false);
+					//console.log("parse_data Fcn://\tget sent for peripheral #: " + j + " type: " + modelParse.config[modelParse.trigger_list[j].port] + "!!!!!");
+					j++;
+				} else {
+					j = 0;
 				}
-			}
-		}
+			}, 1000);
 	},
+
+	handleStop: function () { // not currently working.
+			// stop all motors
+			makeCorsRequest(modelParse.url,'{"status":"set","sm_type":"large motor","port":"outA","info":"stop","value":"break"}', 0, false);
+			clearInterval(delayedLoop); // stop trigger scanning
+
+	},
+
 	is_state_change: function(response, this_trigger, lastVal) {
 		if ((this_trigger.mode == "press_in") && (lastVal != response) && (response == 1)) {
 			console.log("is_state_change Fcn://\t button press state change detected");
@@ -52,40 +63,45 @@ modelParse = {
 			console.log("is_state_change Fcn://\t button release state change detected");
 			return true;
 		}
+		else { 
+			return false;
+		}
 	},
 
 	send_get: function(sensorName,trig,index,isFirst) {
 		/*SEND A REQUEST WITH:*/
-		var get_str = "{'status':'";
-			get_str += "get";
-			get_str += "','sm_type':'";
-			get_str += sensorName;
-			get_str += "','port':'";
-			get_str += parse_port(trig.port);
-			get_str += "','info':'";
-			get_str += trigger_settings(sensorName,trig.settings);
-			get_str += "','value':";
+		var get_str = '{"status":"';
+			get_str += 'get';
+			get_str += '","sm_type":"';
+			get_str += this.parse_name(sensorName);
+			get_str += '","port":"';
+			get_str += this.parse_port(trig.port);
+			get_str += '","info":"';
+			get_str += this.trigger_settings(sensorName,trig.settings);
+			get_str += '","value":';
 			get_str += -1;// dummy value to make this the right number of arguments (EV3 doesn't use value field for "get"s)	
-			get_str += "}";
-			makeCorsRequest(url,get_str,index,isFirst);	
+			get_str += '}';
+			makeCorsRequest(this.url,get_str,index,isFirst);	
 	},
 
 	send_set: function(full_config,action_arr) {
 		var len = action_arr.length;
 		for (var k = 0; k < len; k++) {
 			/*SEND A REQUEST WITH:*/
-			var set_str = "{'status':'";
-			set_str = "set";
-			set_str += "','sm_type':'";
-			set_str += full_config[action_arr[k].port];
-			set_str += "','port':'";
-			set_str += parse_port(action_arr[k].port);
-			set_str += "','info':'";
-			set_str += set_act_info(action_arr[k].mode);
-			set_str += "','value':";
-			set_str += set_act_val(action_arr[k].mode, action_arr[k].settings);
-			set_str += "}";
-			makeCorsRequest(url,set_str,k,false);
+			var set_str = '{"status":"';
+			set_str += 'set';
+			set_str += '","sm_type":"';
+			set_str += this.parse_name(full_config[action_arr[k].port]);
+			set_str += '","port":"';
+			set_str += this.parse_port(action_arr[k].port);
+			set_str += '","info":"';
+			set_str += this.set_act_info(action_arr[k].mode);
+			set_str += '","value":"';
+			set_str += this.set_act_val(action_arr[k].mode, action_arr[k].settings);
+			set_str += '"}';
+			console.log("----------------------------------------------");
+			console.log(set_str);
+			makeCorsRequest(this.url,set_str,k,false);
 		}
 	},
 
@@ -98,7 +114,7 @@ modelParse = {
 		}
 	},
 
-	parseNameTranslations: { // dictionary to convert port names to EV3 readable format
+	parsePortTranslations: { // dictionary to convert port names to EV3 readable format
 		"A":"outA",
 		"B":"outB",
 		"C":"outC",
@@ -108,9 +124,17 @@ modelParse = {
 		"3":"in3",
 		"4":"in4"
 	},
-
 	parse_port: function(portName) {
-		return parseNameTranslations[portName];
+		return this.parsePortTranslations[portName];
+	},
+	parseNameTranslations: {
+		"Touch Sensor":"touch",
+		"Large Motor":"large motor",
+		"Medium Motor":"medium motor"
+	},
+
+	parse_name: function(sm_type) {
+		return this.parseNameTranslations[sm_type];
 	},
 
 	set_act_info: function(mode) {
@@ -134,7 +158,7 @@ modelParse = {
 		}
 		else if (mode == "stop") {
 			if (settings.stopType == "brake") {return "brake";}
-			else if (setting.stopType == "coast") {return "coast";}
+			else if (settings.stopType == "coast") {return "coast";}
 		}
 	}
 }
