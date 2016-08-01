3cdc2425b990f9aa1c33fce0a2dd6b9b5e25aacf
BenZackin <ben.zackin@gmail.com>
2016-08-01 16:59:35 -0400

    Started adding in all EV3 peripherals

--- MAMP-files/data_model_parser3(cors).js
+++ MAMP-files/data_model_parser3(cors).js
@@ -26,15 +26,17 @@ var modelParse = {
 	//			-Sending HTTP requests to retrieve input values and execute output actions.
 	//
 	parse_data: function (dataFile) { 
+		console.log(dataFile);
 		//Start Data Parsing
 		var data = JSON.parse(dataFile);
-		//console.log("parse_data Fcn://\tJSON parsed to variable 'data'");
+		console.log(data);
+		console.log("parse_data Fcn://\tJSON parsed to variable 'data'");
 		var raw_config = data.portConfig;
-		for (var d = 0; d < data.triggers.length; d++) {}
+		//for (var d = 0; d < data.triggers.length; d++) {};
 		this.trigger_list = data.triggers;
-		for (portID in raw_config) { // truncate portConfig to only contain assigned ports
-			if (raw_config[portID] != "None") {
-				this.config[portID] = raw_config[portID];
+		for (portID in data.portConfig) { // truncate portConfig to only contain assigned ports
+			if (data.portConfig[portID] != "None") {
+				this.config[portID] = data.portConfig[portID];
 			}
 		}
 		//console.log("parse_data Fcn://\tport config truncated");
@@ -49,12 +51,12 @@ var modelParse = {
 		// 	console.log("parse_data Fcn://\tinfinite loop entered");
 		// 	j = 0;
 			delayedLoop = setInterval(function(){ 
-				//console.log("parse_data Fcn://\t delayed loop entered, iteration: " + j);
+				console.log("parse_data Fcn://\t delayed loop entered, iteration: " + j);
 
 				if(j < trigger_len) {
 				 	//console.log("parse_data Fcn://\t about to send get for trigger #: " + j);
 					modelParse.send_get(modelParse.config[modelParse.trigger_list[j].port], modelParse.trigger_list[j], j, false);
-					//console.log("parse_data Fcn://\tget sent for peripheral #: " + j + " type: " + modelParse.config[modelParse.trigger_list[j].port] + "!!!!!");
+					console.log("parse_data Fcn://\tget sent for peripheral #: " + j + " type: " + modelParse.config[modelParse.trigger_list[j].port]);
 					j++;
 				} else {
 					j = 0;
@@ -99,7 +101,33 @@ var modelParse = {
 			console.log("is_state_change Fcn://\t button release state change detected");
 			return true;
 		}
-		/*
+		else if ((this_trigger.mode == "enter_range") && (lastVal > this_trigger.threshold) && (response <= this_trigger.threshold)) { // Ultrasonic sensor implementation
+			return true;
+		}
+		else if ((this_trigger.mode == "exit_range") && (lastVal < this_trigger.threshold) && (response >= this_trigger.threshold)) { // Ultrasonic sensor implementation
+			return true;
+		}
+		else if ((this_trigger.mode == "exit_range") && (lastVal < this_trigger.threshold) && (response >= this_trigger.threshold)) { // Ultrasonic sensor implementation
+			return true;
+		}
+		else if ((this_trigger.mode == "position_passes") && (this_trigger.comparisonType == "<") && (lastVal > this_trigger.threshold) && (response <= this_trigger.threshold)) { // Ultrasonic sensor implementation
+			// "degrees" or "rotations"
+			return true; // do we need to differentiate for different units or will juliana send them back in the right units??????????????????????????
+		}
+		else if ((this_trigger.mode == "position_passes") && (this_trigger.comparisonType == ">") && (lastVal < this_trigger.threshold) && (response >= this_trigger.threshold)) { // Ultrasonic sensor implementation
+			// "degrees" or "rotations"
+			return true;// do we need to differentiate for different units or will juliana send them back in the right units??????????????????????????
+		}
+		else if ((this_trigger.mode == "rate_passes") && (this_trigger.comparisonType == "<") && (lastVal > this_trigger.threshold) && (response <= this_trigger.threshold)) { // Ultrasonic sensor implementation
+			// "deg/sec" or "rot/sec"
+			return true; // do we need to differentiate for different units or will juliana send them back in the right units??????????????????????????
+		}
+		else if ((this_trigger.mode == "rate_passes") && (this_trigger.comparisonType == ">") && (lastVal < this_trigger.threshold) && (response >= this_trigger.threshold)) { // Ultrasonic sensor implementation
+			// "deg/sec" or "rot/sec"
+			return true;// do we need to differentiate for different units or will juliana send them back in the right units??????????????????????????
+		}
+
+		/* FORMAT:
 		else if((CONFIRM THE MODE) && (CHECK THAT LAST VALUE DID NOT BREAK THE THRESHOLD) && (CHECK THAT IT BREAKS THE THRESHOLD)) {
 			return true;
 		}
@@ -230,6 +258,13 @@ var modelParse = {
 	//
 	parseNameTranslations: {
 		"Touch Sensor":"touch",
+		"Ultrasonic Sensor":"", // fill these in!!
+		"Color Sensor":"",// fill these in!!
+		"Gyro Sensor":"",// fill these in!!
+		"Brick Button":"",// fill these in!!
+		"Brick Light":"",// fill these in!!
+		"Brick Sound":"",// fill these in!!
+
 		"Large Motor":"large motor",
 		"Medium Motor":"medium motor"
 	},
@@ -255,6 +290,7 @@ var modelParse = {
 		if (mode == "stop") {
 			return "stop";
 		}
+		// add in action info for all others
 	},
 
 	// set_act_val()
@@ -278,5 +314,6 @@ var modelParse = {
 			if (settings.stopType == "brake") {return "brake";}
 			else if (settings.stopType == "coast") {return "coast";}
 		}
+		// add in value info for all others
 	}
 }
